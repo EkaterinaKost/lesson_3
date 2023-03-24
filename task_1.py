c572cd7140880707ccdb263f0424df94fbcfc0d2
ekaterina <kostcat-84@mail.ru>
2023-03-24 22:12:40 +0300

    added something

--- task_1.py
+++ task_1.py
@@ -11,3 +11,15 @@ name = input('ведите ваше имя :')
 password = input('ведите ваш пароль :')
 age = int(input('ведите ваш возраст :'))
 print(f'имя - {name}, пароль - {password}, возраст - {age}')
+n = int(input("введите трехзначное число", ))
+sum_value = 0
+count_it = 0
+while n > 0:
+    x = n % 10
+    sum_value = sum_value + x
+    n = n // 10
+    count_it += 1
+if count_it != 3:
+    print('введено не трхзначное число')
+else:
+    print(sum_value)
